fa211b27cbb5261cfc46d64436cb9a956702fdb1
Ryjen <ryjen.pears@gmail.com>
2021-12-12 20:05:01 +0100

    custom 404 added

--- app.py
+++ app.py
@@ -367,6 +367,12 @@ def homepage():
         return render_template('home-anon.html')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a 404 page when page not found."""
+
+    return render_template('404.html'), 404
+
 ##############################################################################
 # Turn off all caching in Flask
 #   (useful for dev; in production, this kind of stuff is typically
